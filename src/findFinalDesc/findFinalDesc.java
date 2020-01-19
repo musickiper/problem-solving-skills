e01aa4f3f5b7a2d5385e942004e2dc2a5695b134
James Jung <leedo01219@gmail.com>
2020-01-19 03:08:25 -0500

    Optimized Find final destination

--- src/findFinalDesc/findFinalDesc.java
+++ src/findFinalDesc/findFinalDesc.java
@@ -1,8 +1,50 @@
 package findFinalDesc;
-import com.sun.tools.javac.util.Pair;
-import org.omg.PortableInterceptor.ACTIVE;
 
 public class findFinalDesc {
+  public static void main(String[] args) {
+    int startX = 0;
+    int startY = 0;
+    Action[] actions = {Action.FORWARD, Action.BACKWORD, Action.TURNLEFT, Action.FORWARD};
+    findDest(new int[] {startX, startY}, actions);
+  }
+
+  public static void findDest(int[] startLocation, Action[] actions) {
+    Direction direction = Direction.UP;
+    for (Action action : actions) {
+      direction = getUpdatedDirection(startLocation, direction, action);
+    }
+    System.out.println("X = " + startLocation[0] + ", " + "Y = " + startLocation[1]);
+  }
+
+  public static Direction getUpdatedDirection(
+      int[] prevLocation, Direction prevDirection, Action action) {
+    if (prevDirection == Direction.UP) {
+      if (action == Action.FORWARD) prevLocation[0]--;
+      if (action == Action.BACKWORD) prevLocation[0]++;
+      if (action == Action.TURNLEFT) return Direction.LEFT;
+      if (action == Action.TURNRIGHT) return Direction.RIGHT;
+    }
+    if (prevDirection == Direction.DOWN) {
+      if (action == Action.FORWARD) prevLocation[0]++;
+      if (action == Action.BACKWORD) prevLocation[0]--;
+      if (action == Action.TURNLEFT) return Direction.RIGHT;
+      if (action == Action.TURNRIGHT) return Direction.LEFT;
+    }
+    if (prevDirection == Direction.LEFT) {
+      if (action == Action.FORWARD) prevLocation[1]--;
+      if (action == Action.BACKWORD) prevLocation[1]++;
+      if (action == Action.TURNLEFT) return Direction.DOWN;
+      if (action == Action.TURNRIGHT) return Direction.UP;
+    }
+    if (prevDirection == Direction.RIGHT) {
+      if (action == Action.FORWARD) prevLocation[1]++;
+      if (action == Action.BACKWORD) prevLocation[1]--;
+      if (action == Action.TURNLEFT) return Direction.UP;
+      if (action == Action.TURNRIGHT) return Direction.DOWN;
+    }
+    return Direction.UP;
+  }
+
   enum Direction {
     UP,
     DOWN,
@@ -16,68 +58,4 @@ public class findFinalDesc {
     TURNLEFT,
     TURNRIGHT
   };
-
-  public static void main(String[] args) {
-    int startX = 0;
-    int startY = 0;
-    Action[] actions = {Action.FORWARD, Action.BACKWORD, Action.TURNLEFT,
-        Action.FORWARD};
-    findDest(startX, startY, actions);
-  }
-
-  public static void findDest(int startX, int startY, Action[] actions) {
-    Direction direction = Direction.UP;
-    for(Action action : actions) {
-      switch(direction) {
-        case UP:
-          if(action == Action.FORWARD) {
-            startX--;
-          } else if(action == Action.BACKWORD) {
-            startX++;
-          } else if(action == Action.TURNLEFT) {
-            direction = Direction.LEFT;
-          } else if(action == Action.TURNRIGHT) {
-            direction = Direction.RIGHT;
-          }
-          break;
-        case DOWN:
-          if(action == Action.FORWARD) {
-            startX++;
-          } else if(action == Action.BACKWORD) {
-            startX--;
-          } else if(action == Action.TURNLEFT) {
-            direction = Direction.RIGHT;
-          } else if(action == Action.TURNRIGHT) {
-            direction = Direction.LEFT;
-          }
-          break;
-        case LEFT:
-          if(action == Action.FORWARD) {
-            startY--;
-          } else if(action == Action.BACKWORD) {
-            startY++;
-          } else if(action == Action.TURNLEFT) {
-            direction = Direction.DOWN;
-          } else if(action == Action.TURNRIGHT) {
-            direction = Direction.UP;
-          }
-          break;
-        case RIGHT:
-          if(action == Action.FORWARD) {
-            startY++;
-          } else if(action == Action.BACKWORD) {
-            startY--;
-          } else if(action == Action.TURNLEFT) {
-            direction = Direction.UP;
-          } else if(action == Action.TURNRIGHT) {
-            direction = Direction.DOWN;
-          }
-          break;
-        default:
-          break;
-      }
-    }
-    System.out.println(startX);
-    System.out.println(startY);
-  }
 }
